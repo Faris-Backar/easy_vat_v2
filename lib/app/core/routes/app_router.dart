2f3aba33e930f5063fc611ae84688bbb4fc3b9d8
faris-backer <faris.backer@lxisoft.com>
2025-02-13 01:45:02 +0530

    Added shared preference and it resource file

--- lib/app/core/routes/app_router.dart
+++ lib/app/core/routes/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:easy_vat_v2/app/core/resources/pref_resources.dart';
 import 'package:easy_vat_v2/app/core/routes/app_router.gr.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 @AutoRouterConfig(replaceInRouteName: 'Screen|Page,Route')
 class AppRouter extends RootStackRouter {
@@ -16,4 +18,18 @@ class AppRouter extends RootStackRouter {
 
   //pages
   static const String splash = "/";
+}
+
+class AuthGuard extends AutoRouteGuard {
+  @override
+  void onNavigation(NavigationResolver resolver, StackRouter router) async {
+    final prefs = await SharedPreferences.getInstance();
+    final isAuthenticated =
+        prefs.getBool(PrefResources.isAuthenticated) ?? false;
+    if (isAuthenticated) {
+      resolver.next();
+    } else {
+      // router.replaceNamed(AppRouter.loginScreen);
+    }
+  }
 }
