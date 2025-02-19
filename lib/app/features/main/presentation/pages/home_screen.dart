8c2fef752737a0f474d2140bfff85ff836f359f4
faris-backer <faris.backer@lxisoft.com>
2025-02-19 19:43:42 +0530

    Fixed minor UI issues

--- lib/app/features/main/presentation/pages/home_screen.dart
+++ lib/app/features/main/presentation/pages/home_screen.dart
@@ -1,9 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_vat_v2/app/core/app_strings.dart';
 import 'package:easy_vat_v2/app/core/extensions/extensions.dart';
-import 'package:easy_vat_v2/app/core/theme/custom_text_theme.dart';
 import 'package:easy_vat_v2/app/features/main/data/home_data.dart';
-import 'package:easy_vat_v2/app/features/main/presentation/widgets/category_item_card.dart';
+import 'package:easy_vat_v2/app/features/main/presentation/widgets/home_category_menu.dart';
 import 'package:easy_vat_v2/app/features/main/presentation/widgets/quick_item_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -17,7 +16,7 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       appBar: PreferredSize(
         preferredSize:
-            Size(double.infinity, MediaQuery.paddingOf(context).top + 10.h),
+            Size(double.infinity, MediaQuery.paddingOf(context).top + 5.h),
         child: Container(color: Theme.of(context).scaffoldBackgroundColor),
       ),
       body: NestedScrollView(
@@ -53,16 +52,17 @@ class HomeScreen extends StatelessWidget {
                   child: SingleChildScrollView(
                     child: Column(
                       children: [
-                        _buildCategorySection(context, AppStrings.masterData,
-                            HomeData.masterData),
-                        _buildCategorySection(
-                            context,
-                            AppStrings.inventoryVoucher,
-                            HomeData.inventoryVoucher),
-                        _buildCategorySection(context,
-                            AppStrings.accountVoucher, HomeData.accountVoucher),
-                        _buildCategorySection(
-                            context, AppStrings.hrm, HomeData.hrmData),
+                        HomeCategoryMenu(
+                            title: AppStrings.masterData,
+                            dataList: HomeData.masterData),
+                        HomeCategoryMenu(
+                            title: AppStrings.inventoryVoucher,
+                            dataList: HomeData.inventoryVoucher),
+                        HomeCategoryMenu(
+                            title: AppStrings.accountVoucher,
+                            dataList: HomeData.accountVoucher),
+                        HomeCategoryMenu(
+                            title: AppStrings.hrm, dataList: HomeData.hrmData),
                       ],
                     ),
                   ),
@@ -72,33 +72,6 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-
-  Widget _buildCategorySection(
-      BuildContext context, String title, List homeData) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SizedBox(height: 20.h),
-        Text(title, style: CustomTextTheme.header(context)),
-        SizedBox(height: 20.h),
-        GridView.builder(
-          itemCount: homeData.length,
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 4,
-            childAspectRatio: .7,
-            mainAxisSpacing: 15,
-            crossAxisSpacing: 15,
-          ),
-          itemBuilder: (_, index) =>
-              CategoryItemCard(homeData: homeData[index]),
-        ),
-        SizedBox(height: 20.h),
-        const Divider(),
-      ],
     );
   }
 }
---
+++ lib/app/features/main/presentation/widgets/home_category_menu.dart
@@ -0,0 +1,43 @@
+import 'package:easy_vat_v2/app/core/theme/custom_text_theme.dart';
+import 'package:easy_vat_v2/app/features/main/data/home_data.dart';
+import 'package:easy_vat_v2/app/features/main/presentation/widgets/category_item_card.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomeCategoryMenu extends StatelessWidget {
+  final String title;
+  final List<HomeDataModel> dataList;
+  final bool isDividerRequired;
+  const HomeCategoryMenu(
+      {super.key,
+      required this.title,
+      required this.dataList,
+      this.isDividerRequired = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 20.h),
+        Text(title, style: CustomTextTheme.header(context)),
+        SizedBox(height: 20.h),
+        GridView.builder(
+          itemCount: dataList.length,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            childAspectRatio: .7,
+            mainAxisSpacing: 15,
+            crossAxisSpacing: 15,
+          ),
+          itemBuilder: (_, index) =>
+              CategoryItemCard(homeData: dataList[index]),
+        ),
+        if (isDividerRequired) SizedBox(height: 20.h),
+        if (isDividerRequired) const Divider(),
+      ],
+    );
+  }
+}
