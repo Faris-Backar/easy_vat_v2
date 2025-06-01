917a6910693f377ac38e5b29d016f4f9babeec50
Mohamed Hafiz <m.hafizvy6@gmail.com>
2025-06-01 18:06:16 +0400

    ledger add dialog

--- lib/app/features/ledger/presentation/widgets/ledger_add_dialog.dart
+++ lib/app/features/ledger/presentation/widgets/ledger_add_dialog.dart
@@ -1,6 +1,10 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:easy_vat_v2/app/core/app_core.dart';
 import 'package:easy_vat_v2/app/core/extensions/extensions.dart';
+import 'package:easy_vat_v2/app/core/utils/app_utils.dart';
 import 'package:easy_vat_v2/app/features/ledger/domain/entities/ledger_account_entity.dart';
+import 'package:easy_vat_v2/app/features/widgets/primary_button.dart';
+import 'package:easy_vat_v2/app/features/widgets/secondary_button.dart';
 import 'package:easy_vat_v2/app/features/widgets/text_input_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -63,6 +67,30 @@ class _LedgerAddDialogState extends ConsumerState<LedgerAddDialog> {
                 SizedBox(
                   width: 5.w,
                 ),
+                // Expanded(
+                //     child: TextInputFormField(
+                //         height: 36.h,
+                //         label: context.translate(AppStrings.taxAmount),
+                //         fillColor: context.colorScheme.tertiaryContainer,
+                //         enabled: false,
+                //         controller: _taxAmountController)),
+                // SizedBox(
+                //   width: 5.w,
+                // ),
+                // Expanded(
+                //     child: TextInputFormField(
+                //         height: 36.h,
+                //         label: context.translate(AppStrings.netTotal),
+                //         fillColor: context.colorScheme.tertiaryContainer,
+                //         enabled: false,
+                //         controller: _netTotalController))
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              children: [
                 Expanded(
                     child: TextInputFormField(
                         height: 36.h,
@@ -81,10 +109,36 @@ class _LedgerAddDialogState extends ConsumerState<LedgerAddDialog> {
                         enabled: false,
                         controller: _netTotalController))
               ],
+            ),
+            SizedBox(
+              height: 10,
             )
           ],
         ),
       ),
+      contentTextStyle:
+          context.textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.w400),
+      contentPadding:
+          const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+      actions: [
+        SecondaryButton(
+          onPressed: () => context.router.popForced(),
+          label: context.translate(AppStrings.cancel),
+          labelColor: context.defaultTextColor,
+          backgroundColor: AppUtils.isDarkMode(context)
+              ? context.colorScheme.tertiary
+              : null,
+          border: BorderSide(
+              color: context.colorScheme.primary.withValues(alpha: 0.2)),
+        ),
+        SizedBox(
+          width: 5.w,
+        ),
+        PrimaryButton(
+          label: context.translate(AppStrings.addToCart),
+          onPressed: () {},
+        )
+      ],
     );
   }
 }
