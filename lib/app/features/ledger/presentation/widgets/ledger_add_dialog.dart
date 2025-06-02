44f417bcd2b3c293bccb98b435660486efcd5cc0
Mohamed Hafiz <m.hafizvy6@gmail.com>
2025-06-02 12:52:34 +0400

    ledger add dialog widget integrated with calculations

--- lib/app/features/ledger/presentation/widgets/ledger_add_dialog.dart
+++ lib/app/features/ledger/presentation/widgets/ledger_add_dialog.dart
@@ -1,18 +1,23 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_vat_v2/app/core/app_core.dart';
 import 'package:easy_vat_v2/app/core/extensions/extensions.dart';
+import 'package:easy_vat_v2/app/core/resources/pref_resources.dart';
 import 'package:easy_vat_v2/app/core/utils/app_utils.dart';
+import 'package:easy_vat_v2/app/features/cart/domain/entities/cart_entity.dart';
 import 'package:easy_vat_v2/app/features/ledger/domain/entities/ledger_account_entity.dart';
 import 'package:easy_vat_v2/app/features/widgets/primary_button.dart';
 import 'package:easy_vat_v2/app/features/widgets/secondary_button.dart';
 import 'package:easy_vat_v2/app/features/widgets/text_input_form_field.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LedgerAddDialog extends ConsumerStatefulWidget {
   final LedgerAccountEntity ledger;
-  const LedgerAddDialog({super.key, required this.ledger});
+  final CartEntity? ledgerEntry;
+  const LedgerAddDialog({super.key, required this.ledger, this.ledgerEntry});
 
   @override
   ConsumerState<ConsumerStatefulWidget> createState() =>
@@ -20,14 +25,57 @@ class LedgerAddDialog extends ConsumerStatefulWidget {
 }
 
 class _LedgerAddDialogState extends ConsumerState<LedgerAddDialog> {
-  late LedgerAccountEntity ledget;
+  CartEntity? cart;
+  late LedgerAccountEntity ledger;
   final _grossTotalController = TextEditingController();
   final _taxPercentController = TextEditingController();
   final _taxAmountController = TextEditingController();
   final _netTotalController = TextEditingController();
-  double taxAmount = 0.0;
-  double netTotal = 0.0;
+  double taxPercentage = 0.0;
   bool isTaxEnabled = false;
+
+  final _decimalInputFormatter =
+      FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$'));
+
+  @override
+  void initState() {
+    super.initState();
+    cart = widget.ledgerEntry;
+    ledger = widget.ledger;
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final prefs = await SharedPreferences.getInstance();
+      isTaxEnabled = prefs.getBool(PrefResources.isTaxEnabled) ?? false;
+      taxPercentage = isTaxEnabled ? 0.0 : ledger.taxPercentage ?? 0.0;
+    });
+
+    taxPercentage = ledger.taxPercentage ?? 0.0;
+
+    final double grossTotal = 0.0;
+    final double taxAmount = grossTotal * taxPercentage / 100;
+    final double netTotal = grossTotal + taxAmount;
+
+    _grossTotalController.text = grossTotal.toStringAsFixed(2);
+    _taxPercentController.text = taxPercentage.toStringAsFixed(2);
+    _taxAmountController.text = taxAmount.toStringAsFixed(2);
+    _netTotalController.text = netTotal.toStringAsFixed(2);
+  }
+
+  void _updateFromGrossTotalChange() {
+    final grossTotal = double.tryParse(_grossTotalController.text) ?? 0.0;
+    final taxPercentage = double.tryParse(_taxPercentController.text) ?? 0.0;
+    final taxAmount = grossTotal * taxPercentage / 100;
+    final netTotal = grossTotal + taxAmount;
+
+    _taxAmountController.text = taxAmount.toStringAsFixed(2);
+    _netTotalController.text = netTotal.toStringAsFixed(2);
+  }
+
+  // void _updateTaxAmount() {
+  //   final grossTotal = double.parse(_grossTotalController.text);
+  //   final amountWithTax = grossTotal + (grossTotal * taxPercentage / 100);
+  //   _taxAmountController.text = amountWithTax.toStringAsFixed(2);
+  //   _updateFromGrossTotalChange();
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -49,6 +97,17 @@ class _LedgerAddDialogState extends ConsumerState<LedgerAddDialog> {
                       height: 36.h,
                       label: context.translate(AppStrings.grossTotal),
                       fillColor: context.colorScheme.tertiaryContainer,
+                      onChanged: (value) => _updateFromGrossTotalChange(),
+                      textInputAction: TextInputAction.next,
+                      textInputType:
+                          const TextInputType.numberWithOptions(decimal: true),
+                      inputFormatters: [_decimalInputFormatter],
+                      maxLines: 1,
+                      onTap: () => _grossTotalController.selection =
+                          TextSelection(
+                              baseOffset: 0,
+                              extentOffset:
+                                  _grossTotalController.value.text.length),
                       controller: _grossTotalController),
                 ),
                 SizedBox(
@@ -61,6 +120,11 @@ class _LedgerAddDialogState extends ConsumerState<LedgerAddDialog> {
                   fillColor: context.colorScheme.tertiaryContainer,
                   textInputAction: TextInputAction.next,
                   controller: _taxPercentController,
+                  maxLines: 1,
+                  onTap: () => _taxPercentController.selection = TextSelection(
+                      baseOffset: 0,
+                      extentOffset: _taxPercentController.value.text.length),
+                  onChanged: (value) => _updateFromGrossTotalChange(),
                   textInputType:
                       const TextInputType.numberWithOptions(decimal: true),
                 )),
@@ -93,21 +157,24 @@ class _LedgerAddDialogState extends ConsumerState<LedgerAddDialog> {
               children: [
                 Expanded(
                     child: TextInputFormField(
-                        height: 36.h,
-                        label: context.translate(AppStrings.taxAmount),
-                        fillColor: context.colorScheme.tertiaryContainer,
-                        enabled: false,
-                        controller: _taxAmountController)),
+                  height: 36.h,
+                  label: context.translate(AppStrings.taxAmount),
+                  fillColor: context.colorScheme.tertiaryContainer,
+                  enabled: false,
+                  controller: _taxAmountController,
+                )),
                 SizedBox(
                   width: 5.w,
                 ),
                 Expanded(
                     child: TextInputFormField(
-                        height: 36.h,
-                        label: context.translate(AppStrings.netTotal),
-                        fillColor: context.colorScheme.tertiaryContainer,
-                        enabled: false,
-                        controller: _netTotalController))
+                  height: 36.h,
+                  label: context.translate(AppStrings.netTotal),
+                  fillColor: context.colorScheme.tertiaryContainer,
+                  enabled: false,
+                  controller: _netTotalController,
+                  onChanged: (value) => _updateFromGrossTotalChange(),
+                ))
               ],
             ),
             SizedBox(
