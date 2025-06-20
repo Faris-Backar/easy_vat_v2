1ca7a225486474898f18d553f5f235e3b901b887
Mohamed Hafiz <m.hafizvy6@gmail.com>
2025-06-20 10:31:50 +0400

    Contra Screen UI

--- lib/app/features/contra/presentation/pages/contra_screen.dart
+++ lib/app/features/contra/presentation/pages/contra_screen.dart
@@ -1,6 +1,12 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:easy_vat_v2/app/core/app_core.dart';
+import 'package:easy_vat_v2/app/core/extensions/extensions.dart';
+import 'package:easy_vat_v2/app/core/utils/app_utils.dart';
+import 'package:easy_vat_v2/app/features/contra/presentation/widgets/contra_appbar.dart';
+import 'package:easy_vat_v2/app/features/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 @RoutePage()
 class ContraScreen extends ConsumerStatefulWidget {
@@ -11,11 +17,58 @@ class ContraScreen extends ConsumerStatefulWidget {
 }
 
 class _ContraScreenState extends ConsumerState<ContraScreen> {
+  final _searchBarController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Contra"),
+      appBar: ContraAppbar(
+          searchController: _searchBarController, config: ContraAppBarConfig()),
+      bottomNavigationBar: SafeArea(
+        child: Container(
+          color: AppUtils.isDarkMode(context)
+              ? context.colorScheme.tertiaryContainer
+              : context.surfaceColor,
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    context.translate(AppStrings.total),
+                    style: context.textTheme.bodyMedium?.copyWith(
+                        color:
+                            context.defaultTextColor.withValues(alpha: 0.32)),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              PrimaryButton(
+                onPressed: () {},
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(
+                      Icons.add_circle_outline_rounded,
+                      color: Colors.white,
+                    ),
+                    SizedBox(
+                      width: 5.w,
+                    ),
+                    Text(
+                      context.translate(AppStrings.addNew),
+                      style: context.textTheme.bodyMedium?.copyWith(
+                          fontWeight: FontWeight.w600, color: Colors.white),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
